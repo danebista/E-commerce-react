12e566ff822efac2ab23871d834e8a17b38e3195
Your Name <bista.dinank@gmail.com>
2021-06-14 19:08:40 +0545

    Sign Up and Sign In Component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,7 @@ import {Homepage} from './pages/homepage/homepage.component.jsx';
 import {Switch,Route} from 'react-router-dom';
 import Shop from './pages/shop/shop.component.jsx';
 import {HeaderComponent} from './components/header-component/header-component.component.jsx';
-
-function Simba(){
-  return (
-    <div>
-      {`I am Simba`}
-    </div>
-  )
-}
+import {SignInAndSignUpPage} from './pages/sign-up_and_sign-in/sign-up_and_sing-in.component.jsx'
 
 function App() {
   return (
@@ -19,6 +12,7 @@ function App() {
       <Switch>
         <Route exact  path='/' component={Homepage} />
         <Route path='/shop' component={Shop} />
+        <Route path='/signinandsignup' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
