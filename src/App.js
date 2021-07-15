a4a7a707f74d345d685431cf73e83ddf525768a6
Your Name <bista.dinank@gmail.com>
2021-07-16 00:52:36 +0545

    Setting up reducers here

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {connect} from 'react-redux';
 import React, {Component} from 'react';
 import {Switch,Route} from 'react-router-dom';
 import Shop from './pages/shop/shop.component.jsx';
@@ -6,32 +7,27 @@ import {Homepage} from './pages/homepage/homepage.component.jsx';
 import HeaderComponent from './components/header-component/header-component.component.jsx';
 import {SignInAndSignUpPage} from './pages/sign-up_and_sign-in/sign-up_and_sing-in.component.jsx'
 import {auth, createUserProfileDocument } from './firebase/firebase.utils';
-
+import  {setCurrentUser} from './redux/user/user-actions';
 class App extends Component{
-  constructor(){
-    super();
-    this.state={
-      currentUser:null
-    }
-  }
   unsubsribeFromAuth = null
 
   componentDidMount(){
+    const {setCurrentUser} = this.props;
     this.unsubsribeFromAuth=auth.onAuthStateChanged(async userAuth=>{
       if (userAuth){
         const userRef = await createUserProfileDocument(userAuth);  
 
         userRef.onSnapshot(snapShot=>{
-          this.setState({
+          setCurrentUser({
             currentUser: {
               id: snapShot.id,
               ...snapShot.data()
             }
-          },()=> {console.log(this.state)})
-        })
+          })
+        });
       }
 
-      this.setState({currentUser: userAuth});
+      setCurrentUser(userAuth);
     });    
   }
 
@@ -53,4 +49,8 @@ class App extends Component{
   }
 }
 
-export default App;
+const mapDispatchToProps= dispatch =>({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
