59d192ba5fe3512cb8f0223d23db5387d892bac3
Your Name <bista.dinank@gmail.com>
2021-07-18 11:59:38 +0545

    Persistor added to store data to local storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,14 +1,21 @@
  import {combineReducers} from 'redux';
- 
+ import {persistReducer} from 'redux-persist';
  import userReducer from './user/user-reducer';
  import cardReducer from './card/card-reducer';
  import directoryReducer from './directory/directory-reducer';
+ import storage from 'redux-persist/lib/storage';
 
+ const persistConfig = {
+     key:'root',
+     storage,
+     whitelist: ['card']
+ }
 
-
- export default  combineReducers({
+ const rootReducer = combineReducers({
      user: userReducer,
      card: cardReducer,
      directory: directoryReducer
  });
-    
+
+ export default persistReducer(persistConfig, rootReducer);
+ 
